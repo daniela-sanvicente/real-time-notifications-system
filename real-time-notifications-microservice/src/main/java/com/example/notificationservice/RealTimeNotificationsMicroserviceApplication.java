f4da7e667b628036524a90e44e2dd0a9ec716469
WhoIsVera <ev765030@gmail.com>
2024-09-10 12:13:31 -0600

    Add project real-time-notifications-microservice

---
+++ real-time-notifications-microservice/src/main/java/com/example/notificationservice/RealTimeNotificationsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeNotificationsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeNotificationsMicroserviceApplication.class, args);
+	}
+
+}
